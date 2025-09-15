f5907800009a9b16583f8007b82b645dc510357f
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2025-09-15 11:33:32 -0300

    atualiza

--- atividade-01/01-insert-veiculos.sql
+++ atividade-01/01-insert-veiculos.sql
@@ -12,4 +12,6 @@ INSERT INTO VEICULOS ( MARCA, MODELO, ANO, COR, PRECO)
 VALUES (1, 'Porsche', '911', 2024, 'Branco', 15000000),
 (2, 'Lamborghini', 'Huracán', 2023, 'Laranja', 25000000),
 (3, 'Ferrari', 'Purosangue', 2024, 'Vermelho', 30000000);
-(4, 'Mercedes', 'AMG', 2024, 'Azul', 7000000);
+(4, 'Mercedes', 'AMG', 2024, 'Azul', 7000000),
+(5, 'BMW', 'M3', 2023, 'Preto', 6000000),
+(6, 'Audi', 'R8', 2022, 'Prata', 12000000);
