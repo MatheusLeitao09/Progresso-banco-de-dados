cbd9930bccc682146fd2cc5a27f351bad87abca5
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2025-09-29 11:41:17 -0300

    feat: Cria página testes

---
+++ testes/acentos.sql
@@ -0,0 +1,14 @@
+CREATE TABLE testes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+)
+
+INSERT INTO testes (nome)
+VALUES
+('João'),
+('Stéfanny'),
+('Leitão'),
+('Márcia'),
+('Bigodão'),
+('Paquetá'),
+('Joãozin');
